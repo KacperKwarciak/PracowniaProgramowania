cea240c27d3510710abf2b6429f1d650889c42e7
Kacper Kwarciak <93049093+KacperKwarciak@users.noreply.github.com>
2021-11-06 16:21:15 +0100

    Add files via upload

---
+++ 03/30.py
@@ -0,0 +1,12 @@
+n=input("N: ")
+n=int(n)
+num=1
+while num<=n:
+    if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+           
+    num=num+1
